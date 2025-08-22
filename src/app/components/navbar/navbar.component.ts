1e9ec18834d482fa7c37045685ea97025d47a2c7
Matheus Müller <142352374+MM-Muller@users.noreply.github.com>
2025-08-22 16:40:33 -0300

    style(navbar): ajusta ícones da navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,22 +9,26 @@ export class NavbarComponent {
   navItems = [
     {
       id: 'home',
-      icon: 'M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z',
+      name: 'Universo',
+      icon: 'M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8zm0-14c-3.309 0-6 2.691-6 6s2.691 6 6 6 6-2.691 6-6-2.691-6-6-6zm0 10c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4z',
       active: true,
     },
     {
-      id: 'menu',
-      icon: 'M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z',
+      id: 'historia',
+      name: 'História',
+      icon: 'M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z',
       active: false,
     },
     {
-      id: 'favorites',
-      icon: 'M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z',
+      id: 'personagens',
+      name: 'Personagens',
+      icon: 'M16 4c0-1.11.89-2 2-2s2 .89 2 2-.89 2-2 2-2-.89-2-2zm4 18v-6h2.5l-2.54-7.63A1.5 1.5 0 0 0 18.54 7H16.5c-.8 0-1.54.5-1.85 1.26l-1.92 5.76A2 2 0 0 0 14.6 16H16v6h4zM12.5 11.5c.83 0 1.5-.67 1.5-1.5s-.67-1.5-1.5-1.5S11 9.17 11 10s.67 1.5 1.5 1.5zM5.5 6c1.11 0 2-.89 2-2s-.89-2-2-2-2 .89-2 2 .89 2 2 2zm2 16v-7H9l-1.5-4.5A1.5 1.5 0 0 0 6.07 9H4.93a1.5 1.5 0 0 0-1.43 1.07L2 14.5h1.5V22h4z',
       active: false,
     },
     {
-      id: 'settings',
-      icon: 'M12 1v6m0 10v6m11-7h-6m-10 0H1',
+      id: 'objetivo',
+      name: 'Objetivo',
+      icon: 'M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2zm0 4.24l-1.75 3.54-3.91.57 2.83 2.76-.67 3.89L12 15.77l3.5 1.84-.67-3.89 2.83-2.76-3.91-.57L12 6.24z',
       active: false,
     },
   ];
@@ -34,13 +38,10 @@ export class NavbarComponent {
   }
 
   onNavItemClick(clickedItem: any): void {
-    // Remove active from all items
     this.navItems.forEach((item) => (item.active = false));
 
-    // Set clicked item as active
     clickedItem.active = true;
 
-    // Emit event or handle navigation logic
-    console.log(`Navigating to: ${clickedItem.id}`);
+    console.log(`Navigando para: ${clickedItem.name} (${clickedItem.id})`);
   }
 }
