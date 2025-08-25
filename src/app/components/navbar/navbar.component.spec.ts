2f90819f1be064a8c9f4781522eee110179c3182
Matheus Müller <142352374+MM-Muller@users.noreply.github.com>
2025-08-25 16:35:42 -0300

    test(navbar): adiciona teste para função onNavItemClick

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -8,7 +8,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [NavbarComponent]
+      declarations: [NavbarComponent],
     });
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
@@ -17,5 +17,16 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the clicked item as active', () => {
+    expect(component.navItems[0].active).toBe(true);
+    expect(component.navItems[1].active).toBe(false);
+
+    const secondItem = component.navItems[1];
+    component.onNavItemClick(secondItem);
+
+    expect(component.navItems[0].active).toBe(false);
+    expect(component.navItems[1].active).toBe(true);
   });
 });
