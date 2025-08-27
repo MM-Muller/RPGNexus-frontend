40a9d1f4799537cedaae69db47a0b934833ee87c
Matheus Müller <142352374+MM-Muller@users.noreply.github.com>
2025-08-27 12:03:27 -0300

    chore(structure): adiciona pastas 'shared' e 'core' para separar responsabilidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CharacterCardComponent } from './components/character-card/character-card.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { HistoriaComponent } from './features/historia/historia.component';
 import { HomeComponent } from './features/home/home.component';
 import { ObjetivoComponent } from './features/objetivo/objetivo.component';
 import { PersonagensComponent } from './features/personagens/personagens.component';
 
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent,
     HistoriaComponent,
     PersonagensComponent,
     ObjetivoComponent,
-    CharacterCardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CharacterCardComponent } from '../components/character-card/character-card.component';
+
+@NgModule({
+  declarations: [CharacterCardComponent],
+  imports: [CommonModule],
+  exports: [CharacterCardComponent],
+})
+export class SharedModule {}
