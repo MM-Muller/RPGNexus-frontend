61cd890e44ebf4c1d8cb2c12b855c2f8b4723ee4
Matheus Müller <142352374+MM-Muller@users.noreply.github.com>
2025-08-29 13:38:21 -0300

    refactor(historia): refatoramento completo da página de história

--- src/app/features/historia/historia.component.ts
+++ src/app/features/historia/historia.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-historia',
   templateUrl: './historia.component.html',
   styleUrls: ['./historia.component.scss'],
 })
-export class HistoriaComponent {}
+export class HistoriaComponent implements OnInit {
+  ngOnInit(): void {
+    this.createStars();
+  }
+
+  createStars(): void {
+    const starsContainer = document.getElementById('starsContainer');
+    if (starsContainer) {
+      const numberOfStars = 150;
+
+      for (let i = 0; i < numberOfStars; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+
+        const size = Math.random() * 3 + 1;
+        star.style.width = size + 'px';
+        star.style.height = size + 'px';
+
+        star.style.left = Math.random() * 100 + '%';
+        star.style.top = Math.random() * 100 + '%';
+
+        star.style.animationDelay = Math.random() * 3 + 's';
+        star.style.animationDuration = Math.random() * 3 + 2 + 's';
+
+        starsContainer.appendChild(star);
+      }
+    }
+  }
+}
